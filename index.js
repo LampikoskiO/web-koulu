208e366b7bf9ccc36308eb53f39e41445ca55833
onni testi <testi@testi1.com>
2022-01-06 05:28:05 +0200

    päivitys, navbar sticky toimii nyt

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  if (x.classList.contains("responsive")) {
+    x.classList.remove("responsive");
   } else {
-    x.className = "topnav";
+    x.classList.add("responsive");
   }
 }
 
 
-/*
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {navsticky()};
+var topnav;
+var sticky;
+function ready(){
+  topnav = document.getElementById("myTopnav");
+  sticky = topnav.offsetTop;                          
+  window.onscroll = function() {navsticky()};
+}
 
-// Get the navbar
-var topnav = document.getElementById("myTopnav");
-
-// Get the offset position of the navbar
-var sticky = topnav.offsetTop;                          //EI LÖYDÄ offsetTop komentoa?
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function navsticky() {
   if (window.pageYOffset >= sticky) {
-    topnav.classList.add("sticky")                      //EI LÖYDÄ sticky classia?
+    topnav.classList.add("sticky")                      
   } else {
     topnav.classList.remove("sticky");
     console.log("sticky");
   }
 }
-*/
